c2e2baea69b411ecf0653d2977065259166bcdbd
fabbricca <fabb.riccardo@gmail.com>
2025-09-05 11:47:07 +0200

    refactor: exported sendService body to an external utility function processOutboundMessage

--- server/src/modules/internal/v1/messages/send/send.service.ts
+++ server/src/modules/internal/v1/messages/send/send.service.ts
@@ -1,8 +1,7 @@
 import type { FastifyRequest } from "fastify";
 import type { SendInput } from "./send.schema.js";
 
-import type { Message, MessageStatus } from "@messaging-service/types";
-import { getQueue } from "@messaging-service/utils";
+import { processOutboundMessage } from "@/utils/processOutboundMessage.js";
 
 export async function sendService(request: FastifyRequest, input: SendInput) {
   const idempotencyKey = request.headers['idempotency-key'];
@@ -10,22 +9,7 @@ export async function sendService(request: FastifyRequest, input: SendInput) {
   if (!idempotencyKey || typeof idempotencyKey !== 'string')
     throw new Error('Idempotency key is required');
 
-  const external = await request.server.repository.createIdempotencyKey(idempotencyKey);
-
-  if (!external)
-    throw new Error('Failed to create idempotency key');
-
-  const msg: Message = {
-    ...input,
-    externalId: external.key,
-  };
-
-  const outboundMsg = await request.server.repository.createOutboundMessage(msg);
-
-  if (outboundMsg.status === 'QUEUED' as MessageStatus) {
-    const queue = getQueue('messages.dispatch', request.server.redisConnection);
-    /* await */ queue.add('dispatch', outboundMsg);
-  }
+  const outboundMsg = await processOutboundMessage(request, idempotencyKey, input);
 
   return { messageId: outboundMsg.id };
 }
