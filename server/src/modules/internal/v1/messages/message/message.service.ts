18b95475cdeb09748842397f363c51bf8dc9d344
fabbricca <fabb.riccardo@gmail.com>
2025-09-17 19:16:44 +0200

    chore(server): remove unused import

--- server/src/modules/internal/v1/messages/message/message.service.ts
+++ server/src/modules/internal/v1/messages/message/message.service.ts
@@ -1,8 +1,5 @@
 import type { FastifyRequest } from "fastify";
 
-import type { Message, MessageStatus } from "@messaging-service/types";
-import { getQueue } from "@messaging-service/utils";
-
 export async function messageService(request: FastifyRequest<{ Params: { id: string } }>) {
   const id = request.params.id;
   const msg = await request.server.repository.getMessageById(id);
