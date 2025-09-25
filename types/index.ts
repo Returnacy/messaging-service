bf7dd8659b413585d2fec8bf3761b454d5464c58
fabbricca <fabb.riccardo@gmail.com>
2025-09-25 11:51:01 +0200

    fix(types): correct zod export to normal export instead of type

--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,11 @@
-export type { messagePayloadSchema } from './zod/messagePayloadSchema.js';
-export type { messageSchema } from './zod/messageSchema.js';
-export type { payloadRecipientSchema } from './zod/payloadRecipientSchema.js';
-export type { batchSchema } from './zod/batchSchema.js';
-export type { decisiontelecomReceiptSchema } from './zod/decisiontelecomReceiptSchema.js';
-export type { resendReceiptSchema } from './zod/resendReceiptSchema.js';
+export { messagePayloadSchema } from './zod/messagePayloadSchema.js';
+export { messageSchema } from './zod/messageSchema.js';
+export { payloadRecipientSchema } from './zod/payloadRecipientSchema.js';
+export { batchSchema } from './zod/batchSchema.js';
+export { decisiontelecomReceiptSchema } from './zod/decisiontelecomReceiptSchema.js';
+export { resendReceiptSchema } from './zod/resendReceiptSchema.js';
 
-export type { Channel} from './channel.js';
+export type { Channel } from './channel.js';
 export type { Message } from './message.js';
 export type { MessagePayload } from './messagePayload.js';
 export type { MessageStatus } from './messageStatus.js';
