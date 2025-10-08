e8047c3ae1d7dfc7e6fd35210dfbd10072242a20
fabbricca <fabb.riccardo@gmail.com>
2025-10-08 12:33:02 +0200

    fix(server): return 202 if message was queued successfully

--- server/src/modules/internal/v1/messages/messages.controller.ts
+++ server/src/modules/internal/v1/messages/messages.controller.ts
@@ -6,8 +6,8 @@ import { messagesService } from './messages.service.js';
 export async function messagesHandler(request: FastifyRequest, reply: FastifyReply) {
   try {
     const input = messageSchema.parse(request.body);
-
     const res = await messagesService(request, input);
+    return reply.status(202).send(res);
   } catch (error) {
     return reply.status(400).send(error);
   }
